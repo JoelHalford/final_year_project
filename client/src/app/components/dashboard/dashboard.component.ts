d50205edc7fc99ede1c296b8ebedb3a8996c26a9
topsup <topsupp@hotmail.com>
2018-04-10 17:48:08 +0100

    update

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -210,7 +210,8 @@ export class DashboardComponent implements OnInit {
                 //set budgetID to that of current budgets ID
                 this.budgetID = data.budgets[i]._id;
                 //set currentBudget to that of current budgets budget_price
-                this.currentBudget = data.budgets[i].budget_price;                
+                this.currentBudget = data.budgets[i].budget_price;
+                (document.getElementById("budgetNow") as HTMLInputElement).value = this.currentBudget;
                 if (this.priceSpent != data.budgets[i].budget_spent)
                 {//if priceSpent is not equal to the one inside current budgets
                   let budget = 
